8eb6a5a9c017b4a7de847aa5017ee742d5518c50
Bartosz Furtak <bartosz.furtak21@gmail.com>
2017-10-24 18:44:10 +0200

    Incorporate prism into project

---
+++ Screener.Infrastructure/RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screener.Infrastructure
+{
+    public static class RegionNames
+    {
+        public const string TOOLBAR_REGION = "ToolbarRegion";
+        public const string MAIN_CONTENT_REGION = "MainContentRegion";
+    }
+}
---
+++ ScreenerShell/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+
+namespace ScreenerShell
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+    }
+}
